80123e59b6a41ee1810b51e97265c083d48d0593
abhishekchandra2522k <1ac23456789@gmail.com>
2020-11-11 15:19:45 +0530

    Modify main.js

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 /* == Menu Show == */
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
     if(toggle && nav){
-        toggle.addEventListener("click", ()=>){
+        toggle.addEventListener("click", ()=>{
             nav.classList.toggle('show')
-        }
+        })
     }
 }
 
@@ -16,5 +17,50 @@ showMenu('nav-toggle','nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
-    navLink.forEach(n => )
+    // Active Link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove menu Mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+
 }
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+// == Scroll Reveal Animation ==
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+})
+
+// == Scroll Home ==
+
+sr.reveal('.home__title',{})
+sr.reveal('.button',{delay: 100})
+sr.reveal('.home__img',{delay: 200})
+sr.reveal('.home__social',{interval: 100})
+
+// == Scroll About == 
+
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay: 100})
+sr.reveal('.about__text',{delay: 200})
+
+// == Scroll Skills ==
+
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay: 100})
+sr.reveal('.skills__data',{interval: 100})
+sr.reveal('.skills__img',{delay: 200})
+
+// == Scroll Work ==
+sr.reveal('.work__img',{})
+
+
+// == Scroll Contact ==
+sr.reveal('.contact__input',{})
